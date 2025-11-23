2ef2634f7416b69583e407dc9046b19a9075f523
devmuturi <muturinjonge09@gmail.com>
2025-11-23 11:58:15 +0300

    Setup view components

--- app/views/components/application_view_component_preview.rb
+++ app/views/components/application_view_component_preview.rb
@@ -1,3 +1,5 @@
 class ApplicationViewComponentPreview < ViewComponentContrib::Preview::Base
   self.abstract_class = true
+
+  include ApplicationHelper
 end
---
+++ app/views/components/nav_bar/component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class NavBar::Component < ApplicationViewComponent
+end
