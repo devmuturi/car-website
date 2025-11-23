29b9c0f1131db3f5de36e86f78f929d12c4230d4
devmuturi <muturinjonge09@gmail.com>
2025-11-23 18:35:59 +0300

    Fox error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.autoload_paths.reject! { |path| path =~ %r{/lib/generators} }
+  config.autoload_paths.reject! do |path|
+    path.to_s.include?("lib/generators")
+  end
 end
